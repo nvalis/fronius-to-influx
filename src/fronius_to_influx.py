02a0608ac7055da835d2e2257e73f586ad43ec81
nvalis <novalis@protonmail.com>
2024-11-05 21:08:31 +0100

    Initial changes, simplify, update requirements

--- src/fronius_to_influx.py
+++ src/fronius_to_influx.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import datetime
 import json
 from time import sleep
@@ -27,7 +26,9 @@ class FroniusToInflux:
     BACKOFF_INTERVAL = 3
     IGNORE_SUN_DOWN = False
 
-    def __init__(self, client: InfluxDBClient, location: Location, endpoints: List[str], tz: Any) -> None:
+    def __init__(
+        self, client: InfluxDBClient, location: Location, endpoints: List[str], tz: Any
+    ) -> None:
         self.client = client
         self.location = location
         self.endpoints = endpoints
@@ -37,84 +38,98 @@ class FroniusToInflux:
     def get_float_or_zero(self, value: str) -> float:
         internal_data: Dict[Any, Any] = {}
         try:
-            internal_data = self.data['Body']['Data']
+            internal_data = self.data["Body"]["Data"]
         except KeyError:
-            raise WrongFroniusData('Response structure is not healthy.')
-        return float(internal_data.get(value, {}).get('Value', 0))
+            raise WrongFroniusData("Response structure is not healthy.")
+        return float(internal_data.get(value, {}).get("Value", 0))
 
     def translate_response(self) -> List[Dict]:
-        collection = self.data['Head']['RequestArguments']['DataCollection']
-        timestamp = self.data['Head']['Timestamp']
-        if collection == 'CommonInverterData':
+        collection = self.data["Head"]["RequestArguments"]["DataCollection"]
+        timestamp = self.data["Head"]["Timestamp"]
+        if collection == "CommonInverterData":
             return [
                 {
-                    'measurement': 'DeviceStatus',
-                    'time': timestamp,
-                    'fields': {
-                        'ErrorCode': self.data['Body']['Data']['DeviceStatus']['ErrorCode'],
-                        'LEDColor': self.data['Body']['Data']['DeviceStatus']['LEDColor'],
-                        'LEDState': self.data['Body']['Data']['DeviceStatus']['LEDState'],
-                        'MgmtTimerRemainingTime': self.data['Body']['Data']['DeviceStatus']['MgmtTimerRemainingTime'],
-                        'StateToReset': self.data['Body']['Data']['DeviceStatus']['StateToReset'],
-                        'StatusCode': self.data['Body']['Data']['DeviceStatus']['StatusCode'],
-                    }
+                    "measurement": "DeviceStatus",
+                    "time": timestamp,
+                    "fields": {
+                        "ErrorCode": self.data["Body"]["Data"]["DeviceStatus"][
+                            "ErrorCode"
+                        ],
+                        "LEDColor": self.data["Body"]["Data"]["DeviceStatus"][
+                            "LEDColor"
+                        ],
+                        "LEDState": self.data["Body"]["Data"]["DeviceStatus"][
+                            "LEDState"
+                        ],
+                        "MgmtTimerRemainingTime": self.data["Body"]["Data"][
+                            "DeviceStatus"
+                        ]["MgmtTimerRemainingTime"],
+                        "StateToReset": self.data["Body"]["Data"]["DeviceStatus"][
+                            "StateToReset"
+                        ],
+                        "StatusCode": self.data["Body"]["Data"]["DeviceStatus"][
+                            "StatusCode"
+                        ],
+                    },
                 },
                 {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
-                        'FAC': self.get_float_or_zero('FAC'),
-                        'IAC': self.get_float_or_zero('IAC'),
-                        'IDC': self.get_float_or_zero('IDC'),
-                        'PAC': self.get_float_or_zero('PAC'),
-                        'UAC': self.get_float_or_zero('UAC'),
-                        'UDC': self.get_float_or_zero('UDC'),
-                        'DAY_ENERGY': self.get_float_or_zero('DAY_ENERGY'),
-                        'YEAR_ENERGY': self.get_float_or_zero('YEAR_ENERGY'),
-                        'TOTAL_ENERGY': self.get_float_or_zero('TOTAL_ENERGY'),
-                    }
-                }
+                    "measurement": collection,
+                    "time": timestamp,
+                    "fields": {
+                        "FAC": self.get_float_or_zero("FAC"),
+                        "IAC": self.get_float_or_zero("IAC"),
+                        "IDC": self.get_float_or_zero("IDC"),
+                        "PAC": self.get_float_or_zero("PAC"),
+                        "UAC": self.get_float_or_zero("UAC"),
+                        "UDC": self.get_float_or_zero("UDC"),
+                        "DAY_ENERGY": self.get_float_or_zero("DAY_ENERGY"),
+                        "YEAR_ENERGY": self.get_float_or_zero("YEAR_ENERGY"),
+                        "TOTAL_ENERGY": self.get_float_or_zero("TOTAL_ENERGY"),
+                    },
+                },
             ]
-        elif collection == '3PInverterData':
+        elif collection == "3PInverterData":
             return [
                 {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
-                        'IAC_L1': self.get_float_or_zero('IAC_L1'),
-                        'IAC_L2': self.get_float_or_zero('IAC_L2'),
-                        'IAC_L3': self.get_float_or_zero('IAC_L3'),
-                        'UAC_L1': self.get_float_or_zero('UAC_L1'),
-                        'UAC_L2': self.get_float_or_zero('UAC_L2'),
-                        'UAC_L3': self.get_float_or_zero('UAC_L3'),
-                    }
+                    "measurement": collection,
+                    "time": timestamp,
+                    "fields": {
+                        "IAC_L1": self.get_float_or_zero("IAC_L1"),
+                        "IAC_L2": self.get_float_or_zero("IAC_L2"),
+                        "IAC_L3": self.get_float_or_zero("IAC_L3"),
+                        "UAC_L1": self.get_float_or_zero("UAC_L1"),
+                        "UAC_L2": self.get_float_or_zero("UAC_L2"),
+                        "UAC_L3": self.get_float_or_zero("UAC_L3"),
+                    },
                 }
             ]
-        elif collection == 'MinMaxInverterData':
+        elif collection == "MinMaxInverterData":
             return [
                 {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
-                        'DAY_PMAX': self.get_float_or_zero('DAY_PMAX'),
-                        'DAY_UACMAX': self.get_float_or_zero('DAY_UACMAX'),
-                        'DAY_UDCMAX': self.get_float_or_zero('DAY_UDCMAX'),
-                        'YEAR_PMAX': self.get_float_or_zero('YEAR_PMAX'),
-                        'YEAR_UACMAX': self.get_float_or_zero('YEAR_UACMAX'),
-                        'YEAR_UDCMAX': self.get_float_or_zero('YEAR_UDCMAX'),
-                        'TOTAL_PMAX': self.get_float_or_zero('TOTAL_PMAX'),
-                        'TOTAL_UACMAX': self.get_float_or_zero('TOTAL_UACMAX'),
-                        'TOTAL_UDCMAX': self.get_float_or_zero('TOTAL_UDCMAX'),
-                    }
+                    "measurement": collection,
+                    "time": timestamp,
+                    "fields": {
+                        "DAY_PMAX": self.get_float_or_zero("DAY_PMAX"),
+                        "DAY_UACMAX": self.get_float_or_zero("DAY_UACMAX"),
+                        "DAY_UDCMAX": self.get_float_or_zero("DAY_UDCMAX"),
+                        "YEAR_PMAX": self.get_float_or_zero("YEAR_PMAX"),
+                        "YEAR_UACMAX": self.get_float_or_zero("YEAR_UACMAX"),
+                        "YEAR_UDCMAX": self.get_float_or_zero("YEAR_UDCMAX"),
+                        "TOTAL_PMAX": self.get_float_or_zero("TOTAL_PMAX"),
+                        "TOTAL_UACMAX": self.get_float_or_zero("TOTAL_UACMAX"),
+                        "TOTAL_UDCMAX": self.get_float_or_zero("TOTAL_UDCMAX"),
+                    },
                 }
             ]
         else:
             raise DataCollectionError("Unknown data collection type.")
 
-
     def sun_is_shining(self) -> None:
         sun = self.location.sun()
-        if not self.IGNORE_SUN_DOWN and not sun['sunrise'] < datetime.datetime.now(tz=self.tz) < sun['sunset']:
+        if (
+            not self.IGNORE_SUN_DOWN
+            and not sun["sunrise"] < datetime.datetime.now(tz=self.tz) < sun["sunset"]
+        ):
             raise SunIsDown
         return None
 
@@ -129,17 +144,18 @@ class FroniusToInflux:
                         self.data = response.json()
                         collected_data.extend(self.translate_response())
                         sleep(self.BACKOFF_INTERVAL)
-                    self.client.write_points(collected_data)
-                    print('Data written')
+                    #self.client.write_points(collected_data)
+                    print(collected_data)
+                    print("Data written")
                     sleep(self.BACKOFF_INTERVAL)
                 except SunIsDown:
                     print("Waiting for sunrise")
                     sleep(60)
-                    print('Waited 60 seconds for sunrise')
+                    print("Waited 60 seconds for sunrise")
                 except ConnectionError:
                     print("Waiting for connection...")
                     sleep(10)
-                    print('Waited 10 seconds for connection')
+                    print("Waited 10 seconds for connection")
                 except Exception as e:
                     self.data = {}
                     sleep(10)
---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fronius_to_influx import FroniusToInflux
+from influxdb import InfluxDBClient
+from astral import Location
+
+import pytz
+
+
+client = InfluxDBClient(url="localhost", token="==")
+location = Location(("Poznan", "Europe", 52.408078, 16.933618, "Europe/Warsaw", 87))
+tz = pytz.timezone("Europe/Warsaw")
+endpoints = [
+    "http://172.30.1.11:5000/3PInverterData.json",
+    "http://172.30.1.11:5000/CommonInverterData.json",
+    "http://172.30.1.11:5000/MinMaxInverterData.json",
+]
+
+z = FroniusToInflux(client, location, endpoints, tz)
+z.IGNORE_SUN_DOWN = True
+z.run()
